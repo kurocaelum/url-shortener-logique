dd1f0b0b39623400d54cc052086e7c25d6d738be
Victor <victor_brandao@outlook.com>
2021-07-22 15:29:19 -0300

    DataSeeder

---
+++ src/main/java/com/logique/urlshortener/seed/DataSeeder.java
@@ -0,0 +1,26 @@
+package com.logique.urlshortener.seed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.logique.urlshortener.model.User;
+import com.logique.urlshortener.repository.UserRepository;
+
+@Component
+public class DataSeeder implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User("user", passwordEncoder.encode("password"));
+		userRepository.save(user);
+	}
+
+}
